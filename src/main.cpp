fc7ed557c946d6226c4cfadd80248f5feb5f63d2
Julien ROUX <julien.roux@ens-paris-saclay.fr>
2019-07-09 18:41:55 +0900

    adding comments in main

--- src/main.cpp
+++ src/main.cpp
@@ -7,100 +7,120 @@
 
 using namespace std;
 
-/*
-Beginning of a new journey in the (scary) worlf of C++
-*/
 
 int main()
 {
 
-    clock_t start, end;
-    double cpu_time_used;
+    /*
+    mode 1 : generate one robot and compute its stability polyhedron
+        results can be displayed using postprocess.py with python3
+    mode 2 : generate a timming "benchmark"
+        results can be displayed using timming_postprocess.py with python3
 
-    start = clock();
-    Robot robot("../robots/robot_2.xml", 16, 200);
-    // robot.buildStabilityProblem();
-    robot.buildReducedStabilityProblem();
-    robot.projectionStabilityPolyhedron();
+    for both mode 3 robots are available:
+    robot_1.xml
+    robot_2.xml
+    robot_3.xml
+    robot_4.xml
 
-    end = clock();
+    only robot_2.xml has 4 accelerations
+    */
+    int mode = 1;
 
-    robot.exportVertices();
 
-    cpu_time_used = ((double) (end - start)) / (CLOCKS_PER_SEC/1000);
+    if (mode == 1)
+    {
+        clock_t start, end;
+        double cpu_time_used;
 
-    std::cout << "Computation time: " << cpu_time_used << "ms for " << robot.get_numberOfVertices() << " inner Vertices"
-              << " and " << robot.get_numberOfOuterVertices() << " outer vertices."<< '\n';
-    std::cout << "Number of Inner Faces: " << robot.get_numberOfFaces() << ", number of outer faces: " << robot.get_numberOfOuterFaces() << '\n';
+        start = clock();
+        Robot robot("../robots/robot_2.xml", 16, 50);
+        // robot.buildStabilityProblem();
+        robot.buildReducedStabilityProblem();
+        robot.projectionStabilityPolyhedron();
 
-    std::cout << "LP time: " << robot.get_lpMicro() << " microseconds" << '\n';
-    std::cout << "inner time: " << robot.get_innerConvexMicro() << " microseconds" << '\n';
-    std::cout << "outer time: " << robot.get_outerConvexMicro() << " microseconds" << '\n';
-    std::cout << "support time: " << robot.get_supportFunctionMicro() << " microseconds" << '\n';
+        end = clock();
 
-    // std::cout << "Testing stuff..." << '\n';
-    //
-    // int const maxNumberOfIterations(300);
-    // int const numberOfTrials(10);
-    //
-    // std::vector<double> lpTimes(maxNumberOfIterations);
-    // std::vector<double> innerConvexTimes(maxNumberOfIterations);
-    // std::vector<double> outerConvexTimes(maxNumberOfIterations);
-    // std::vector<double> supportFunctionTimes(maxNumberOfIterations);
-    //
-    // // std::vector<Robot> robots(maxNumberOfIterations*numberOfTrials, Robot("../robots/robot_2.xml", 16));
-    //
-    // double lpTime, innerTime, outerTime, supportTime;
-    //
-    // for (int numIt=0; numIt<maxNumberOfIterations; numIt++)
-    // {
-    //     lpTime = 0;
-    //     innerTime = 0;
-    //     outerTime = 0;
-    //     supportTime = 0;
-    //
-    //     // robot.set_maxNumberOfIterations(numIt);
-    //        // std::cout << "Reached here n!" << '\n';
-    //     for (int numTrial = 0; numTrial<numberOfTrials; numTrial ++)
-    //     {
-    //         std::cout << "------------------"<< numIt<< " "<< numTrial << "--------------------" << '\n';
-    //         std::unique_ptr<Robot> testRobot(new Robot("../robots/robot_2.xml", 64, numIt));
-    //         testRobot->buildReducedStabilityProblem();
-    //         // testRobot->buildStabilityProblem();
-    //         testRobot->projectionStabilityPolyhedron();
-    //
-    //         lpTime += testRobot->get_lpMicro();
-    //         innerTime += testRobot->get_innerConvexMicro();
-    //         outerTime += testRobot->get_outerConvexMicro();
-    //         supportTime += testRobot->get_supportFunctionMicro();
-    //
-    //     }
-    //
-    //     lpTimes.at(numIt) = lpTime/numberOfTrials;
-    //     innerConvexTimes.at(numIt) = innerTime/numberOfTrials;
-    //     outerConvexTimes.at(numIt) = outerTime/numberOfTrials;
-    //     supportFunctionTimes.at(numIt) = supportTime/numberOfTrials;
-    // }
-    //
-    // ofstream file_stream("timings.txt");
-    //
-    // if (file_stream)
-    // {
-    //     for (int numIt = 0; numIt<maxNumberOfIterations; numIt++)
-    //     {
-    //         file_stream << numIt << ';'
-    //                     << lpTimes.at(numIt) << ';'
-    //                     << innerConvexTimes.at(numIt) << ';'
-    //                     << outerConvexTimes.at(numIt) << ';'
-    //                     << supportFunctionTimes.at(numIt) << ';' << endl;
-    //     }
-    // }
-    // else
-    // {
-    //     std::cerr << "Error: Impossible to open the output file." << '\n';
-    // }
-    //
-    // std::cout << "Stuff tested, use timing_postprocess.py to check the results!" << '\n';
+        robot.exportVertices();
+
+        cpu_time_used = ((double) (end - start)) / (CLOCKS_PER_SEC/1000);
+
+        std::cout << "Computation time: " << cpu_time_used << "ms for " << robot.get_numberOfVertices() << " inner Vertices"
+                  << " and " << robot.get_numberOfOuterVertices() << " outer vertices."<< '\n';
+        std::cout << "Number of Inner Faces: " << robot.get_numberOfFaces() << ", number of outer faces: " << robot.get_numberOfOuterFaces() << '\n';
+
+        std::cout << "LP time: " << robot.get_lpMicro() << " microseconds" << '\n';
+        std::cout << "inner time: " << robot.get_innerConvexMicro() << " microseconds" << '\n';
+        std::cout << "outer time: " << robot.get_outerConvexMicro() << " microseconds" << '\n';
+        std::cout << "support time: " << robot.get_supportFunctionMicro() << " microseconds" << '\n';
+
+    }
+    else if (mode == 2)
+    {
+        std::cout << "Testing stuff..." << '\n';
+
+        int const maxNumberOfIterations(50);
+        int const numberOfTrials(10);
+
+        std::vector<double> lpTimes(maxNumberOfIterations);
+        std::vector<double> innerConvexTimes(maxNumberOfIterations);
+        std::vector<double> outerConvexTimes(maxNumberOfIterations);
+        std::vector<double> supportFunctionTimes(maxNumberOfIterations);
+
+        // std::vector<Robot> robots(maxNumberOfIterations*numberOfTrials, Robot("../robots/robot_2.xml", 16));
+
+        double lpTime, innerTime, outerTime, supportTime;
+
+        for (int numIt=0; numIt<maxNumberOfIterations; numIt++)
+        {
+            lpTime = 0;
+            innerTime = 0;
+            outerTime = 0;
+            supportTime = 0;
+
+            // robot.set_maxNumberOfIterations(numIt);
+               // std::cout << "Reached here n!" << '\n';
+            for (int numTrial = 0; numTrial<numberOfTrials; numTrial ++)
+            {
+                std::cout << "------------------"<< numIt<< " "<< numTrial << "--------------------" << '\n';
+                std::unique_ptr<Robot> testRobot(new Robot("../robots/robot_2.xml", 16, numIt));
+                testRobot->buildReducedStabilityProblem();
+                // testRobot->buildStabilityProblem();
+                testRobot->projectionStabilityPolyhedron();
+
+                lpTime += testRobot->get_lpMicro();
+                innerTime += testRobot->get_innerConvexMicro();
+                outerTime += testRobot->get_outerConvexMicro();
+                supportTime += testRobot->get_supportFunctionMicro();
+
+            }
+
+            lpTimes.at(numIt) = lpTime/numberOfTrials;
+            innerConvexTimes.at(numIt) = innerTime/numberOfTrials;
+            outerConvexTimes.at(numIt) = outerTime/numberOfTrials;
+            supportFunctionTimes.at(numIt) = supportTime/numberOfTrials;
+        }
+
+        ofstream file_stream("timings.txt");
+
+        if (file_stream)
+        {
+            for (int numIt = 0; numIt<maxNumberOfIterations; numIt++)
+            {
+                file_stream << numIt << ';'
+                            << lpTimes.at(numIt) << ';'
+                            << innerConvexTimes.at(numIt) << ';'
+                            << outerConvexTimes.at(numIt) << ';'
+                            << supportFunctionTimes.at(numIt) << ';' << endl;
+            }
+        }
+        else
+        {
+            std::cerr << "Error: Impossible to open the output file." << '\n';
+        }
+
+        std::cout << "Stuff tested, use timing_postprocess.py to check the results!" << '\n';
+    }
 
 
     return 0;
--- timing_postprocess.py
+++ timing_postprocess.py
@@ -35,6 +35,7 @@ ax1.legend()
 
 ax2 = plt.subplot(222)
 ax2.plot(iterations, inner_times, label="time Inner Convex Hull", color="xkcd:ochre")
+# ax2.plot(iterations, outer_times, label="time Outer Double Description Method", color="xkcd:grass")
 ax2.set_xlabel("Number of Iterations")
 ax2.set_ylabel("Time (microseconds)")
 ax2.legend()
@@ -50,6 +51,36 @@ ax4.plot(iterations, support_times, label="time Support Function", color="xkcd:e
 ax4.set_xlabel("Number of Iterations")
 ax4.set_ylabel("Time (microseconds)")
 ax4.legend()
+
+# nlogn = []
+# nsquare = []
+# nlognsquare = []
+# for it in iterations:
+#     nlogn.append(it*np.log(it))
+#     nsquare.append(it*it)
+#     nlognsquare.append(it*np.log(it)**2)
+#
+#
+# ax1 = plt.subplot(221)
+# ax1.plot(iterations, support_times, label="time Support Function", color="xkcd:electric blue")
+# ax1.set_xlabel("n with n the number of iteration")
+# ax1.set_ylabel("Time (microseconds)")
+# ax1.legend()
+# ax2 = plt.subplot(222)
+# ax2.plot(nsquare, support_times, label="time Support Function", color="xkcd:electric blue")
+# ax2.set_xlabel("n**2 with n the number of iteration")
+# ax2.set_ylabel("Time (microseconds)")
+# ax2.legend()
+# ax3 = plt.subplot(223)
+# ax3.plot(nlogn, support_times, label="time Support Function", color="xkcd:electric blue")
+# ax3.set_xlabel("n*log(n) with n the number of iteration")
+# ax3.set_ylabel("Time (microseconds)")
+# ax3.legend()
+# ax4 = plt.subplot(224)
+# ax4.plot(nlognsquare, support_times, label="time Support Function", color="xkcd:electric blue")
+# ax4.set_xlabel("n*log(n)**2 with n the number of iteration")
+# ax4.set_ylabel("Time (microseconds)")
+# ax4.legend()
 
 plt.show()
 
